bdda8a5a43fc1b3d7c5e25e7ade9f810a94b170c
20260139 <20260139@vision.hoseo.edu>
2026-06-10 14:44:55 +0900

    Delete 창프 기말 directory

--- 창프 기말/scripts/game_manager.py
+++
@@ -1,9 +0,0 @@
-# scripts/game_manager.py
-
-class GameManager:
-    def __init__(self):
-        self.state          = "LOBBY"
-        self.score          = 0
-        self.coins          = 0
-        self.reroll_tickets = 0
-        self.stage          = 1   # 현재 스테이지 (1~5)
